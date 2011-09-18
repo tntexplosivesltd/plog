e5caa8dfdb95ba3ca591bc72730e4260f2f9c547
Thomas Phillips <tntexplosivesltd@gmail.com>
2011-09-18 14:03:15 +1200

    Changed new page a bit

--- new_post/new.php
+++ new_post/new.php
@@ -3,23 +3,17 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
     <meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
-    <link href="../../main.css" rel="stylesheet" type="text/css" />
-    <title>IRC - entropy.net.nz</title>
+    <title>New Post</title>
   </head>
 
 <body>
-  <?php include("navbar.htm"); ?>
-    <div id="wrapper">
-      <div id="entropymain">
-        <h1>New Post</h1>
-        <p>
-          <form action="new_post.cgi" method="POST">
-            Title: <input type="text" name="title"><br />
-            Post: <br /><textarea rows="10" cols="100" name="body" />
-            <input type="sumbit" value="Post">
-          </form>
-        </p>
-      </div>
-    </div>
-  </body>
+  <h1>New Post</h1>
+  <p>
+    <form action="new_post.cgi" method="POST">
+      Title: <input type="text" name="title"><br />
+      Post: <br /><textarea rows="10" cols="100" name="body"></textarea>
+      <input type="sumbit" value="Post">
+    </form>
+  </p>
+</body>
 </html>
